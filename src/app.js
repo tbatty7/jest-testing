ade6e1cd9ebd539534e44e3da932473ba1c20f43
Tim Batty <tbatty7@gmail.com>
2024-03-12 13:44:55 -0400

    added first fizzbuzz test

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 export function add(a, b) {
   return a + b;
 }
+
+export function subtract(a, b) {
+  return a - b;
+}
+export function fizzbuzz(a) {
+  return a;
+}
--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,15 @@
-import { add } from "./app";
+import { add, fizzbuzz, subtract } from "./app";
 
 describe("my test suite", () => {
-  it("first test", () => {
+  it("adds 1+1", () => {
     expect(add(1, 1)).toEqual(2);
   });
+  it("subtracts 2-1", () => {
+    expect(subtract(2, 1)).toEqual(1);
+  });
+  it("passing 1 to fizzbuzz returns 1", () => {
+    const extracted = fizzbuzz(1);
+    expect(extracted).toEqual(1);
+  });
+
 });
