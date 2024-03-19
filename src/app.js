99b7d31dd720da5711c5cf2cda7315dd805eb009
Tim Batty <tbatty7@gmail.com>
2024-03-19 19:43:46 -0400

    reverted for video

--- src/app.js
+++ src/app.js
@@ -2,9 +2,3 @@ export function add(a, b) {
   return a + b;
 }
 
-export function subtract(a, b) {
-  return a - b;
-}
-export function fizzbuzz(a) {
-  return a;
-}
--- src/app.test.js
+++ src/app.test.js
@@ -1,15 +1,7 @@
-import { add, fizzbuzz, subtract } from "./app";
+import { add } from "./app";
 
 describe("my test suite", () => {
   it("adds 1+1", () => {
     expect(add(1, 1)).toEqual(2);
   });
-  it("subtracts 2-1", () => {
-    expect(subtract(2, 1)).toEqual(1);
-  });
-  it("passing 1 to fizzbuzz returns 1", () => {
-    const extracted = fizzbuzz(1);
-    expect(extracted).toEqual(1);
-  });
-
 });
