8f7b057d2c1d2bd52645e8f701101389ae2c8131
Tim Batty <tbatty7@gmail.com>
2024-03-19 20:51:37 -0400

    added fizzbuzz and arithmetic functions

--- src/app.js
+++ src/app.js
@@ -2,3 +2,27 @@ export function add(a, b) {
   return a + b;
 }
 
+export function subtract(a, b) {
+  return a - b;
+}
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export function divide(a, b) {
+  return a / b;
+}
+
+export function fizzbuzz(a) {
+  if (a % 15 === 0) {
+    return "fizzbuzz";
+  }
+  if (a % 3 === 0) {
+    return "fizz";
+  }
+  if (a % 5 === 0) {
+    return "buzz";
+  }
+  return a;
+}
--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,41 @@
-import { add } from "./app";
+import { add, subtract, multiply, divide, fizzbuzz } from "./app";
 
 describe("my test suite", () => {
   it("adds 1+1", () => {
     expect(add(1, 1)).toEqual(2);
+  });
+  it("subtracts 2-1", () => {
+    expect(subtract(2, 1)).toEqual(1);
+  });
+  it("multiplies 2 and 2", () => {
+    expect(multiply(2, 2)).toEqual(4);
+  });
+  it("divides 10 by 2", () => {
+    expect(divide(10, 2)).toEqual(5);
+  });
+
+  it("fizzbuzz 1 returns 1", () => {
+    const result = fizzbuzz(1);
+    expect(result).toEqual(1);
+  });
+  it("fizzbuzz 2 returns 2", () => {
+    const result = fizzbuzz(2);
+    expect(result).toEqual(2);
+  });
+  it("fizzbuzz 3 returns fizz", () => {
+    const result = fizzbuzz(3);
+    expect(result).toEqual("fizz");
+  });
+  it("fizzbuzz 6 returns fizz", () => {
+    const result = fizzbuzz(6);
+    expect(result).toEqual("fizz");
+  });
+  it("fizzbuzz 5 returns buzz", () => {
+    const result = fizzbuzz(5);
+    expect(result).toEqual("buzz");
+  });
+  it("fizzbuzz 15 returns fizzbuzz", () => {
+    const result = fizzbuzz(15);
+    expect(result).toEqual("fizzbuzz");
   });
 });
